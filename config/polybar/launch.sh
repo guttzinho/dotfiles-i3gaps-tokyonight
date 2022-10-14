17fcda032e924b14ce63f87fa6e8439affa02e3a
guttzinho <gu7theguy@protonmail.com>
2022-10-14 13:57:06 -0300

    modified:   config/i3/config 	modified:   config/polybar/config.ini 	modified:   config/polybar/launch.sh

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -2,10 +2,8 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
 
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
+# Start new bat instances
 polybar -r mybar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
